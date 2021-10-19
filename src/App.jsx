cb3b116c378048ae838a5a9d3d4b6613e4e3b09f
sakilk130 <sakilk130@gmail.com>
2021-10-19 22:40:15 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles/App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello from react</h1>
+      {/* Header */}
+      <Header />
+
+      {/* Sidebar */}
+      {/* Recommended Video */}
+    </div>
+  );
+}
+
+export default App;
